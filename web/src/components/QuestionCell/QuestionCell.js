1276d382363473ab933e8d08617465f056186a0b
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-06-16 20:57:46 +0000

    question cell

---
+++ web/src/components/QuestionCell/QuestionCell.js
@@ -0,0 +1,19 @@
+export const QUERY = gql`
+  query FindQuestionQuery($id: Int!) {
+    question: question(id: $id) {
+      id
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => (
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
+)
+
+export const Success = ({ question }) => {
+  return <div>{JSON.stringify(question)}</div>
+}
